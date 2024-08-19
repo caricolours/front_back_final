6fedbbec7aef0093c4a3f17c749e80b5a9385d88
caricolours <109998560+caricolours@users.noreply.github.com>
2024-08-19 12:38:42 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export const URLBASE = 'http://localhost:3000'
+
+
+export const URLRENDER = 'https://front-back-final.onrender.com'
+export const ENDPOINT = {
+    login: `${URLRENDER}/login`,
+    users: `${URLRENDER}/usuarios`,
+    publicaciones: `${URLRENDER}/publicaciones`,
+    compras: `${URLRENDER}/compras`
+  }
+  
